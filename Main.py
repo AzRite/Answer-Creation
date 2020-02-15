34d3f7eeede31cd36840fe93a410884c11d8edc1
AzR1te <61073707+AzR1te@users.noreply.github.com>
2020-02-15 16:10:45 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -51,11 +51,11 @@ def handle_message(event):
 
 @handler.add(MemberJoinedEvent)
 def handle_join(event):
-    uId = event.joined.members[0].userId
-    gId = event.source.groupId
+    #uId = event.joined.members[0].userId
+    #gId = event.source.groupId
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text="こんにちは、{},{}。".format(uId, gId)))
+        TextSendMessage(text="こんにちは、{}。".format(event.joined)))
 
 
 
