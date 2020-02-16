5d0d2154ad5c07a592948d5792af7992c68d7c98
AzR1te <61073707+AzR1te@users.noreply.github.com>
2020-02-16 14:54:58 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -82,7 +82,6 @@ def handle_message(event):
                                            thumbnail_image_url=profile.picture_url,
                                            title=profile.display_name,
                                            text=f"User ID: {profile.user_id[:10]}..."
-                                                #f"Status Message: {status_msg}",
                                            actions=[MessageAction(label="User IDを取得", text=profile.user_id)]))
         line_bot_api.reply_message(event.reply_token, messages=messages)
     else:
