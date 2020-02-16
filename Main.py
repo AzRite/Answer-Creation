84ecd309b87f1388f735ed9ebe03c59a6912213d
AzR1te <61073707+AzR1te@users.noreply.github.com>
2020-02-16 14:06:20 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -81,9 +81,9 @@ def handle_message(event):
                                        template=ButtonsTemplate(
                                            thumbnail_image_url=profile.picture_url,
                                            title=profile.display_name,
-                                           text=f"User Id: {profile.user_id[:5]}...\n"
+                                           text=f"User ID: {profile.user_id}\n"
                                                 f"Status Message: {status_msg}",
-                                           actions=[MessageAction(label="成功", text="次は何を実装しましょうか？")]))
+                                           actions=[MessageAction(label="User IDを取得", text={profile.user_id})]))
         line_bot_api.reply_message(event.reply_token, messages=messages)
     else:
         text=cmd + "コマンドは存在しません"
